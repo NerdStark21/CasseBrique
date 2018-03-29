4bee5d401763b0f88e7b91387d2972928ddc2cff
Cousin Florian <florian.cousin@telecom-st-etienne.fr>
2018-03-29 11:37:50 +0200

    Model 0.07 - Headers à jour

--- CasseBriqueQt/hardstuff.h
+++ CasseBriqueQt/hardstuff.h
@@ -19,25 +19,28 @@ public:
      * @brief isTouched
      * Cette fonction permet de savoir si l'objet est en colision avec la boule ou non
      *
+     * @param ball
+     *  Pointeur vers la boule à considérer
+     *
      * @return true si la boule a une partie qui touche l'objet et false sinon
      */
-    virtual bool isTouched(Ball *) = 0;
+    virtual bool isTouched(const Ball * ball) const = 0;
 
     /**
      * @brief updateAngle
      *  Cette fonction permet de mettre à jour l'attribut angle_ de la ball
      * @param ball
-     *  pointeur vers la boule à mettre à jour
+     *  Pointeur vers la boule à mettre à jour
      */
-    virtual void updateAngle(Ball * ball) = 0;
+    virtual void updateAngle(Ball * ball) const = 0;
 
     /**
      * @brief updatePosition
      *  Cette fonction permet de mettre à jour l'attribut position_ de la ball
      * @param ball
-     *  pointeur vers la boule à mettre à jour
+     *  Pointeur vers la boule à mettre à jour
      */
-    virtual void updatePosition(Ball * ball) = 0;
+    virtual void updatePosition(Ball * ball) const = 0;
 };
 
 #endif // HARDSTUFF_H
--- CasseBriqueQt/model.h
+++ CasseBriqueQt/model.h
@@ -1,11 +1,34 @@
 #ifndef MODEL_H
 #define MODEL_H
 
+#include "brick.h"
+#include "disk.h"
+#include "player.h"
+#include "wall.h"
+
+#include <vector>
+#include <List>
+
+using namespace std;
 
 class Model
 {
+    vector<Wall> walls_; // Contient les 4 murs du jeu
+    list<Brick> bricks_; // Contient toutes les briques du jeu
+    Disk disk_; // Contient le disque du jeu
+    Ball ball_; // Contient la balle du jeu
+    Player player_; // Contient les informations sur le joueur
+
 public:
     Model();
+
+    /**
+     * @brief updateBall
+     *  Cette fonction actualise le jeu après un certain temps sans actualisation
+     * @param time
+     *  temps écoulé depuis la dernière actualisation
+     */
+    void updateGame(const float time);
 };
 
 #endif // MODEL_H
